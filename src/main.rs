54fab86c3fb964a94e347b1291077c765c60124c
Iskren Hadzhinedev <iskren.hadzhinedev@x3me.net>
2023-11-16 13:23:36 +0200

    support for various flow records

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use byteorder::{BigEndian, ReadBytesExt};
+use mac_address::MacAddress;
 use std::sync::mpsc;
 use std::thread;
 use std::{io::Cursor, io::Read, io::Seek, net::IpAddr, net::UdpSocket, time::Duration};
@@ -23,6 +24,7 @@ impl SFlowRecord {
     }
 }
 
+#[derive(Debug)]
 struct SFlowRawPacketHeader {
     protocol: u32,
     frame_length: u32,
@@ -30,12 +32,52 @@ struct SFlowRawPacketHeader {
     header_size: u32,
     header: Vec<u8>,
 }
-
-struct SFlowEthernetFrame {
-    frame_length: u32,
-    payload: Vec<u8>,
+impl From<SFlowRecord> for SFlowRawPacketHeader {
+    fn from(record: SFlowRecord) -> Self {
+        let mut buf = Cursor::new(record.data);
+        let protocol = buf.read_u32::<BigEndian>().unwrap();
+        let frame_length = buf.read_u32::<BigEndian>().unwrap();
+        let stripped = buf.read_u32::<BigEndian>().unwrap();
+        let header_size = buf.read_u32::<BigEndian>().unwrap();
+        let mut header = vec![0; header_size as usize];
+        buf.read_exact(&mut header).unwrap();
+        SFlowRawPacketHeader {
+            protocol,
+            frame_length,
+            stripped,
+            header_size,
+            header,
+        }
+    }
 }
 
+#[derive(Debug)]
+struct SFlowEthernetFrame {
+    frame_length: u32,
+    source_mac: MacAddress,
+    destination_mac: MacAddress,
+    ethertype: u16,
+}
+impl From<SFlowRecord> for SFlowEthernetFrame {
+    fn from(record: SFlowRecord) -> Self {
+        let mut buf = Cursor::new(record.data);
+        let frame_length = buf.read_u32::<BigEndian>().unwrap();
+        let mut mac_buf = [0; 6];
+        buf.read_exact(&mut mac_buf).unwrap();
+        let source_mac = MacAddress::new(mac_buf);
+        buf.read_exact(&mut mac_buf).unwrap();
+        let destination_mac = MacAddress::new(mac_buf);
+        let ethertype = buf.read_u16::<BigEndian>().unwrap();
+        SFlowEthernetFrame {
+            frame_length,
+            source_mac,
+            destination_mac,
+            ethertype,
+        }
+    }
+}
+
+#[derive(Debug)]
 struct SFlowIpv4 {
     length: u32,
     protocol: u32,
@@ -46,7 +88,31 @@ struct SFlowIpv4 {
     tcp_flags: u32,
     tos: u32,
 }
+impl From<SFlowRecord> for SFlowIpv4 {
+    fn from(record: SFlowRecord) -> Self {
+        let mut buf = Cursor::new(record.data);
+        let length = buf.read_u32::<BigEndian>().unwrap();
+        let protocol = buf.read_u32::<BigEndian>().unwrap();
+        let src_ip = IpAddr::V4(buf.read_u32::<BigEndian>().unwrap().into());
+        let dst_ip = IpAddr::V4(buf.read_u32::<BigEndian>().unwrap().into());
+        let src_port = buf.read_u32::<BigEndian>().unwrap();
+        let dst_port = buf.read_u32::<BigEndian>().unwrap();
+        let tcp_flags = buf.read_u32::<BigEndian>().unwrap();
+        let tos = buf.read_u32::<BigEndian>().unwrap();
+        SFlowIpv4 {
+            length,
+            protocol,
+            src_ip,
+            dst_ip,
+            src_port,
+            dst_port,
+            tcp_flags,
+            tos,
+        }
+    }
+}
 
+#[derive(Debug)]
 struct SFlowIpv6 {
     length: u32,
     protocol: u32,
@@ -57,18 +123,77 @@ struct SFlowIpv6 {
     tcp_flags: u32,
     priority: u32,
 }
+impl From<SFlowRecord> for SFlowIpv6 {
+    fn from(record: SFlowRecord) -> Self {
+        let mut ipv6_buf: [u8; 16] = [0; 16];
+        let mut buf = Cursor::new(record.data);
+        let length = buf.read_u32::<BigEndian>().unwrap();
+        let protocol = buf.read_u32::<BigEndian>().unwrap();
+        buf.read_exact(&mut ipv6_buf).unwrap();
+        let src_ip = IpAddr::V6(ipv6_buf.into());
+        buf.read_exact(&mut ipv6_buf).unwrap();
+        let dst_ip = IpAddr::V6(ipv6_buf.into());
+        let src_port = buf.read_u32::<BigEndian>().unwrap();
+        let dst_port = buf.read_u32::<BigEndian>().unwrap();
+        let tcp_flags = buf.read_u32::<BigEndian>().unwrap();
+        let priority = buf.read_u32::<BigEndian>().unwrap();
+        SFlowIpv6 {
+            length,
+            protocol,
+            src_ip,
+            dst_ip,
+            src_port,
+            dst_port,
+            tcp_flags,
+            priority,
+        }
+    }
+}
 
+#[derive(Debug)]
 struct SFlowExtendedSwitch {
     src_vlan: u32,
     src_priority: u32,
     dst_vlan: u32,
     dst_priority: u32,
 }
+impl From<SFlowRecord> for SFlowExtendedSwitch {
+    fn from(record: SFlowRecord) -> Self {
+        let mut buf = Cursor::new(record.data);
+        let src_vlan = buf.read_u32::<BigEndian>().unwrap();
+        let src_priority = buf.read_u32::<BigEndian>().unwrap();
+        let dst_vlan = buf.read_u32::<BigEndian>().unwrap();
+        let dst_priority = buf.read_u32::<BigEndian>().unwrap();
+        SFlowExtendedSwitch {
+            src_vlan,
+            src_priority,
+            dst_vlan,
+            dst_priority,
+        }
+    }
+}
 
+// TODO: Implement
+#[derive(Debug)]
 struct SFlowExtendedRouter {
     nexthop: IpAddr,
     src_mask: u32,
     dst_mask: u32,
+}
+impl From<SFlowRecord> for SFlowExtendedRouter {
+    fn from(record: SFlowRecord) -> Self {
+        let mut buf = Cursor::new(record.data);
+        let mut nexthop_buf: [u8; 16] = [0; 16];
+        buf.read_exact(&mut nexthop_buf).unwrap();
+        let nexthop = IpAddr::V6(nexthop_buf.into());
+        let src_mask = buf.read_u32::<BigEndian>().unwrap();
+        let dst_mask = buf.read_u32::<BigEndian>().unwrap();
+        SFlowExtendedRouter {
+            nexthop,
+            src_mask,
+            dst_mask,
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -191,7 +316,43 @@ fn main() {
     let th = thread::spawn(move || loop {
         let mut c = Cursor::new(rx.recv().unwrap());
         let datagram = SFlowDatagram::parse(&mut c);
-        // println!("{:?}", datagram);
+        println!("{}: {}\n", datagram.agent_address, datagram.num_samples);
+        for sample in datagram.samples {
+            for record in sample.records {
+                match record.record_type {
+                    1 => {
+                        let raw_packet_header = SFlowRawPacketHeader::from(record);
+                        println!("Raw Packet Header: {:?}\n", raw_packet_header);
+                    }
+                    2 => {
+                        let ethernet_frame = SFlowEthernetFrame::from(record);
+                        println!(
+                            "Ethernet Frame: length: {}, source: {}, destination: {}\n",
+                            ethernet_frame.frame_length,
+                            ethernet_frame.source_mac,
+                            ethernet_frame.destination_mac,
+                        );
+                    }
+                    3 => {
+                        let ipv4 = SFlowIpv4::from(record);
+                        println!("IPv4: {:?}\n", ipv4);
+                    }
+                    4 => {
+                        let ipv6 = SFlowIpv6::from(record);
+                        println!("IPv6: {:?}\n", ipv6);
+                    }
+                    1001 => {
+                        let extended_switch = SFlowExtendedSwitch::from(record);
+                        println!("Extended Switch: {:?}\n", extended_switch);
+                    }
+                    1002 => {
+                        let extended_router = SFlowExtendedRouter::from(record);
+                        println!("Extended Router: {:?}\n", extended_router);
+                    }
+                    _ => println!("Unknown record type"),
+                }
+            }
+        }
     });
     loop {
         let (amt, src) = socket.recv_from(&mut buf).expect("didn't receive data");
